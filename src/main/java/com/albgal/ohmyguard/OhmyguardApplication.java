d4881d5daf3750c00c8124a1dbe95e73f30433eb
Mohamed <malbgal@gmail.com>
2020-11-17 21:04:28 -0800

    first commit

---
+++ src/main/java/com/albgal/ohmyguard/OhmyguardApplication.java
@@ -0,0 +1,13 @@
+package com.albgal.ohmyguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhmyguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhmyguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albgal/ohmyguard/OhmyguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albgal.ohmyguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhmyguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
