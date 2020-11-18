e4dbb27d156e6256b70a4c698efa8149b3e59e19
Mohamed <malbgal@gmail.com>
2020-11-17 21:06:50 -0800

    bismillah

--- src/main/java/com/albgal/ohmyguard/OhmyguardApplication.java
+++ src/main/java/com/albgal/ohmyguard/OhmyguardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OhmyguardApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Bismillah");
 		SpringApplication.run(OhmyguardApplication.class, args);
 	}
 
